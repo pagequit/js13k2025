420b879c5caa501bb14f3c85715821147607d4d1
kolaflash <kolaflash@GAMBIT>
2025-08-12 16:05:05 +0200

    WIP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import zzfx from "./zzfx.js";
+import ZzFX from "./zzfx.js";
+
+let zzfx = ZzFX({
+  vol: 0.3,
+  ctx: new AudioContext(),
+});
 
 document.addEventListener("click", () => {
-  // zzfx(...[,,925,.04,.3,.6,1,.3,,6.27,-184,.09,.17]); // Game Over
-  // zzfx(...[,,537,.02,.02,.22,1,1.59,-6.98,4.97]); // Heart
-  zzfx.play(...[1.5, 0.8, 270, , 0.1, , 1, 1.5, , , , , , , , 0.1, 0.01]); // Piano
-  // zzfx.play(...[,,129,.01,,.15,,,,,,,,5]); // Drum
+  zzfx(...[1.5, 0.8, 270, , 0.1, , 1, 1.5, , , , , , , , 0.1, 0.01]);
 });
 
 let main = "main";
