06c8b0ab984530ee44b8fd209662a916a78ca716
kolaflash <kolaflash@GAMBIT>
2025-08-14 11:39:22 +0200

    Deal with SVG

--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
 import song from "./song";
-import { useZzFX } from "./zzfx";
+import useZzFX from "./zzfx";
 
-canvas.width = 360;
-canvas.height = 740;
-let ctx = canvas.getContext("2d");
+app.width = 360;
+app.height = 740;
+let ctx = app.getContext("2d");
 ctx.imageSmoothingEnabled = false;
+
+let _ = null;
+let svgNS = "http://www.w3.org/2000/svg";
+
+let doc = document;
+let body = doc.body;
+let cE = doc.createElementNS.bind(doc);
+
+function createSVG(d) {
+  let svg = cE(svgNS, "svg");
+  let sSA = body.setAttributeNS.bind(svg);
+  sSA(_, "width", 24);
+  sSA(_, "height", 24);
+  sSA(_, "viewBox", "0 0 24 24");
+
+  let path = cE(svgNS, "path");
+  let pSA = body.setAttributeNS.bind(path);
+  pSA(_, "fill", "none");
+  pSA(_, "stroke", "currentColor");
+  pSA(_, "strokeWidth", 2);
+  pSA(_, "d", d);
+  svg.appendChild(path);
+
+  return [
+    svg,
+    (d) => {
+      pSA(_, "d", d);
+    },
+  ];
+}
+
+let playIcon = "M7 4v16l13-8z";
+let pauseIcon =
+  "M6 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm8 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z";
+let [ppButton, setPPButton] = createSVG(playIcon);
+wrap.appendChild(ppButton);
 
 let bgm = {
   v: 0.3,
@@ -17,7 +53,7 @@ let sfx = {
   x: new AudioContext(),
 };
 
-let [_, zzfxP, zzfxM] = useZzFX(bgm);
+let [__, zzfxP, zzfxM] = useZzFX(bgm);
 let [zzfx] = useZzFX(sfx);
 
 let buffer = zzfxM(...song);
@@ -28,7 +64,7 @@ let node;
 // zzfx(...[.7,,404,.03,.02,.07,1,1.4,1,88,,,,.8,,,,.77,.05,,169]); // jump
 
 let unPaused = !!node;
-self.addEventListener("click", async () => {
+app.addEventListener("click", async () => {
   // prettier-ignore
   zzfx(...[1.7,,61,.2,,.04,3,.4,,,150,.07,.01,,,,.12,.67]);
   if ((unPaused = !unPaused)) {
@@ -55,6 +91,9 @@ let fps = 1000 / 60;
       ctx.font = "24px monospace";
       ctx.fillStyle = "white";
       ctx.fillText("Paused", 136, 360);
+      setPPButton(playIcon);
+    } else {
+      setPPButton(pauseIcon);
     }
   }
   requestAnimationFrame(animate);
--- src/zzfx.js
+++ src/zzfx.js
@@ -1,5 +1,5 @@
 // ZzFX Music Renderer v2.0.3 by Keith Clark and Frank Force
-export function useZzFX(cf) {
+export default (cf) => {
   // prettier-ignore
   let zzfxP=(...t)=>{let e=cf.x.createBufferSource(),f=cf.x.createBuffer(t.length,t[0].length,cf.r);t.map((d,i)=>f.getChannelData(i).set(d)),e.buffer=f,e.connect(cf.x.destination),e.start();return e};
   // prettier-ignore
@@ -10,4 +10,4 @@ export function useZzFX(cf) {
     zzfxP,
     (n,f,t,e=125)=>{let l,o,z,r,g,h,x,a,u,c,d,i,m,p,G,M=0,R=[],b=[],j=[],k=0,q=0,s=1,v={},w=cf.r/e*60>>2;for(;s;k++)R=[s=a=d=m=0],t.map((e,d)=>{for(x=f[e][k]||[0,0,0],s|=!!f[e][k],G=m+(f[e][0].length-2-!a)*w,p=d==t.length-1,o=2,r=m;o<x.length+p;a=++o){for(g=x[o],u=o==x.length+p-1&&p||c!=(x[0]||0)|g|0,z=0;z<w&&a;z++>w-99&&u?i+=(i<1)/99:0)h=(1-i)*R[M++]/2||0,b[r]=(b[r]||0)-h*q+h,j[r]=(j[r++]||0)+h*q+h;g&&(i=g%1,q=x[1]||0,(g|=0)&&(R=v[[c=x[M=0]||0,g]]=v[[c,g]]||(l=[...n[c]],l[2]*=2**((g-12)/12),g>0?zzfxG(...l):[])))}m=G});return[b,j]},
   ]
-}
+};
